7ed2e8d04a5529f999d8ad0a6ad7a9b8768623de
Kuba Suder <jakub.suder@gmail.com>
2023-08-30 01:48:32 +0200

    created gem from a template

---
+++ lib/minisky/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Minisky
+  VERSION = "0.1.0"
+end
---
+++ spec/minisky_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Minisky do
+  it "has a version number" do
+    expect(Minisky::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
