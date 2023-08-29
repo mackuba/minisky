88b06ce0b759e713e8275c99d079171cec1770c8
Kuba Suder <jakub.suder@gmail.com>
2023-08-30 01:52:47 +0200

    updated auto-generated files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,1 @@
 ## [Unreleased]
-
-## [0.1.0] - 2023-08-30
-
-- Initial release
--- spec/minisky_spec.rb
+++ spec/minisky_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Minisky do
   it "has a version number" do
     expect(Minisky::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
