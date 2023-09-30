41d3207247a27e4eb8107d4a580dfc718a94be34
Kuba Suder <jakub.suder@gmail.com>
2023-10-01 01:40:13 +0200

    tests for missing id/pass

--- spec/custom_client_spec.rb
+++ spec/custom_client_spec.rb
@@ -23,18 +23,64 @@ end
 describe "custom client" do
   include FakeFS::SpecHelpers
 
-  before do
-    File.write('test.json', %({
-      "id": "john.foo",
-      "pass": "hunter2",
-      "access_token": "aatoken",
-      "refresh_token": "rrtoken"
-    }))
-  end
+  let(:data) {{
+    'id' => 'john.foo',
+    'pass' => 'hunter2',
+    'access_token' => 'aatoken',
+    'refresh_token' => 'rrtoken'
+  }}
 
   subject { CustomJSONClient.new }
 
   let(:reloaded_config) { JSON.parse(File.read('test.json')) }
 
-  include_examples "Requests", 'at.x.com'
+  context 'with correct config' do
+    before do
+      File.write('test.json', JSON.generate(data))
+    end
+
+    include_examples "Requests", 'at.x.com'
+  end
+
+  context '#log_in' do
+    context 'if id field is nil' do
+      before do
+        File.write('test.json', JSON.generate(data.merge('id' => nil)))
+      end
+
+      it 'should raise AuthError' do
+        expect { subject.log_in }.to raise_error(Minisky::AuthError)
+      end
+    end
+
+    context 'if id field is not included' do
+      before do
+        File.write('test.json', JSON.generate(data.slice('pass', 'access_token', 'refresh_token')))
+      end
+
+      it 'should raise AuthError' do
+        expect { subject.log_in }.to raise_error(Minisky::AuthError)
+      end
+    end
+
+    context 'if pass field is nil' do
+      before do
+        File.write('test.json', JSON.generate(data.merge('pass' => nil)))
+      end
+
+      it 'should raise AuthError' do
+        expect { subject.log_in }.to raise_error(Minisky::AuthError)
+      end
+    end
+
+    context 'if pass field is not included' do
+      before do
+        File.write('test.json', JSON.generate(data.slice('id', 'access_token', 'refresh_token')))
+      end
+
+      it 'should raise AuthError' do
+        expect { subject.log_in }.to raise_error(Minisky::AuthError)
+      end
+    end
+  end
 end
--- spec/minisky_spec.rb
+++ spec/minisky_spec.rb
@@ -5,22 +5,66 @@ describe Minisky do
 
   let(:host) { 'bsky.test' }
 
-  before do
-    File.write('myconfig.yml', %(
-      id: john.foo
-      pass: hunter2
-      access_token: aatoken
-      refresh_token: rrtoken
-    ))
-  end
-
   subject { Minisky.new(host, 'myconfig.yml') }
 
   let(:reloaded_config) { YAML.load(File.read('myconfig.yml')) }
+
+  let(:data) {{
+    'id' => 'john.foo',
+    'pass' => 'hunter2',
+    'access_token' => 'aatoken',
+    'refresh_token' => 'rrtoken'
+  }}
 
   it 'should have a version number' do
     Minisky::VERSION.should_not be_nil
   end
 
-  include_examples "Requests", 'bsky.test'
+  context 'with correct config' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data))
+    end
+
+    include_examples "Requests", 'bsky.test'
+  end
+
+  context 'if id field is nil' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data.merge('id' => nil)))
+    end
+
+    it 'should raise AuthError' do
+      expect { subject }.to raise_error(Minisky::AuthError)
+    end
+  end
+
+  context 'if id field is not included' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data.slice('pass', 'access_token', 'refresh_token')))
+    end
+
+    it 'should raise AuthError' do
+      expect { subject }.to raise_error(Minisky::AuthError)
+    end
+  end
+
+  context 'if pass field is nil' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data.merge('pass' => nil)))
+    end
+
+    it 'should raise AuthError' do
+      expect { subject }.to raise_error(Minisky::AuthError)
+    end
+  end
+
+  context 'if pass field is not included' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data.slice('id', 'access_token', 'refresh_token')))
+    end
+
+    it 'should raise AuthError' do
+      expect { subject }.to raise_error(Minisky::AuthError)
+    end
+  end
 end
