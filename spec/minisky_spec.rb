935b11b02f3f754a595837dd92914a67f969eaab
Kuba Suder <jakub.suder@gmail.com>
2023-10-02 01:19:37 +0200

    tests for auth defaults

--- spec/minisky_spec.rb
+++ spec/minisky_spec.rb
@@ -1,38 +1,22 @@
 require 'yaml'
 require_relative 'shared/ex_unauthed'
 
-describe 'in Minisky instance' do
-  include FakeFS::SpecHelpers
+data = {
+  'id' => 'john.foo',
+  'pass' => 'hunter2',
+  'access_token' => 'aatoken',
+  'refresh_token' => 'rrtoken'
+}.freeze
 
-  let(:host) { 'bsky.test' }
+host = 'bsky.test'
+
+describe Minisky do
+  include FakeFS::SpecHelpers
 
   subject { Minisky.new(host, 'myconfig.yml') }
 
-  let(:reloaded_config) { YAML.load(File.read('myconfig.yml')) }
-
-  let(:data) {{
-    'id' => 'john.foo',
-    'pass' => 'hunter2',
-    'access_token' => 'aatoken',
-    'refresh_token' => 'rrtoken'
-  }}
-
   it 'should have a version number' do
     Minisky::VERSION.should_not be_nil
-  end
-
-  context 'with correct config,' do
-    before do
-      File.write('myconfig.yml', YAML.dump(data))
-    end
-
-    include_examples "Requests", 'bsky.test'
-  end
-
-  context 'without a config' do
-    subject { Minisky.new(host, nil) }
-
-    include_examples "unauthenticated user"
   end
 
   context 'if id field is nil' do
@@ -73,5 +57,53 @@ describe 'in Minisky instance' do
     it 'should raise AuthError' do
       expect { subject }.to raise_error(Minisky::AuthError)
     end
+  end
+
+  context 'with correct config' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data))
+    end
+
+    it 'should send auth headers by default' do
+      subject.send_auth_headers.should == true
+    end
+
+    it 'should manage tokens by default' do
+      subject.auto_manage_tokens.should == true
+    end
+  end
+
+  context 'without a config' do
+    subject { Minisky.new(host, nil) }
+
+    it 'should not send auth headers' do
+      subject.send_auth_headers.should == false
+    end
+
+    it 'should not manage tokens' do
+      subject.auto_manage_tokens.should == false
+    end
+  end
+end
+
+describe 'in Minisky instance' do
+  include FakeFS::SpecHelpers
+
+  subject { Minisky.new(host, 'myconfig.yml') }
+
+  let(:reloaded_config) { YAML.load(File.read('myconfig.yml')) }
+
+  context 'with correct config,' do
+    before do
+      File.write('myconfig.yml', YAML.dump(data))
+    end
+
+    include_examples "Requests", 'bsky.test'
+  end
+
+  context 'without a config' do
+    subject { Minisky.new(host, nil) }
+
+    include_examples "unauthenticated user"
   end
 end
