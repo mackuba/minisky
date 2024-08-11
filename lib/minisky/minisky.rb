db2e72581e8be94e2b3307b9e6c958d287604d07
Kuba Suder <jakub.suder@gmail.com>
2024-08-11 03:18:39 +0200

    set progress to '.' by default when inside irb

--- lib/minisky/minisky.rb
+++ lib/minisky/minisky.rb
@@ -19,11 +19,21 @@ class Minisky
       @auto_manage_tokens = false
     end
 
+    if active_repl?
+      @default_progress = '.'
+    end
+
     if options
       options.each do |k, v|
         self.send("#{k}=", v)
       end
     end
+  end
+
+  def active_repl?
+    return true if defined?(IRB) && IRB.respond_to?(:CurrentContext) && IRB.CurrentContext
+    return true if defined?(Pry) && Pry.respond_to?(:cli) && Pry.cli
+    false
   end
 
   def save_config
