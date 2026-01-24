076fefc98c044ac63ca14ccb29e5526c23df00c8
Kuba Suder <jakub.suder@gmail.com>
2026-01-24 23:52:54 +0200

    added simplecov for measuring test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter "/spec/"
+end
+
 require 'minisky'
 require 'pp' # needs to be included before fakefs
 
@@ -12,5 +19,24 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
+  end
+end
+
+module SimpleCov
+  module Formatter
+    class HTMLFormatter
+      def format(result)
+        # silence the stdout summary, just save the html files
+        unless @inline_assets
+          Dir[File.join(@public_assets_dir, "*")].each do |path|
+            FileUtils.cp_r(path, asset_output_path, remove_destination: true)
+          end
+        end
+
+        File.open(File.join(output_path, "index.html"), "wb") do |file|
+          file.puts template("layout").result(binding)
+        end
+      end
+    end
   end
 end
